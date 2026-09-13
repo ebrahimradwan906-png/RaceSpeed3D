f3fb1d7a540934a3c56c615e5f88b8863a940250
ebrahimradwan906-png <ebrahimradwan906@gmail.com>
2026-09-13 14:29:41 +0300

    Initial 3D racing game

---
+++ app/src/main/java/com/example/racespeed3d/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.racespeed3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView web = new WebView(this);
+        web.setWebViewClient(new WebViewClient());
+
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        web.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        web.loadUrl("file:///android_asset/index.html");
+        setContentView(web);
+    }
+}
